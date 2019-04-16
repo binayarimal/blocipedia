fa66d5ed063205d0aa6ff491d967f99287754686
Binaya Rimal <rimal.binaya@gmail.com>
2019-04-16 16:15:08 -0700

    testing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,41 +1,43 @@
 const userQueries = require("../db/userQueries.js");
-
 const passport = require("passport");
 module.exports = {
   signUp(req, res, next){
     res.render("users/signUp");
   },
 
-  confirmedCreate(req,res,next){
-     let newUser = {
-       email: req.body.email,
-       password: req.body.password,
-       passwordConfirmation: req.body.password_conf
-     };
-     userQueries.createUser(newUser, (err, user) => {
-       if(err){
-         req.flash("error", err);
-         res.redirect("/users/signUp");
-       } else {
-         passport.authenticate("local")(req, res, () => {
-           res.flash("notice", "You've successfully signed in!");
-           res.redirect("/");
-         })
-       };
-     });
 
-   },
+
+
+
   create(req, res, next){
-console.log(this);
-    const sgMail = require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    to: req.body.email,
-    from: 'rimal.binaya@gmail.com',
-    subject: 'Email Confirmation',
-    text: 'Thanks for creqting your account click the button below to confirm',
-    html: `<button onclick = ${this.confirmedCreate(req,res,next)} >Confirm</button>`,
-  };
-  sgMail.send(msg);
+    let newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirmation: req.body.password_conf
+    };
+    userQueries.createUser(newUser, (err, user) => {
+      if(err){
+        req.flash("error", err);
+        res.redirect("/users/signUp");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          req.flash("notice", "You've successfully signed in!");
+          res.redirect("/");
+        });
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+        to: req.body.email,
+        from: 'rimal.binaya@gmail.com',
+        subject: 'Email Confirmation',
+        text: "You've successfully created an account on Blocipedia",
+
+        };
+        sgMail.send(msg);
+
+
+      };
+    });
+
   },
 }
