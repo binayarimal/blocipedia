14eaef58d6e8fc8eb8f116f3c126b796c27bc568
Binaya Rimal <rimal.binaya@gmail.com>
2019-04-16 13:52:44 -0700

    confirmation email

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,34 +6,36 @@ module.exports = {
     res.render("users/signUp");
   },
 
+  confirmedCreate(req,res,next){
+     let newUser = {
+       email: req.body.email,
+       password: req.body.password,
+       passwordConfirmation: req.body.password_conf
+     };
+     userQueries.createUser(newUser, (err, user) => {
+       if(err){
+         req.flash("error", err);
+         res.redirect("/users/signUp");
+       } else {
+         passport.authenticate("local")(req, res, () => {
+           res.flash("notice", "You've successfully signed in!");
+           res.redirect("/");
+         })
+       };
+     });
+
+   },
   create(req, res, next){
-    let newUser = {
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirmation: req.body.password_conf
-    };
-    userQueries.createUser(newUser, (err, user) => {
-      if(err){
-        req.flash("error", err);
-        res.redirect("/users/signUp");
-      } else {
-        const sgMail = require('@sendgrid/mail');
-      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-      const msg = {
-        to: req.body.email,
-        from: 'rimal.binaya@gmail.com',
-        subject: 'Email Confirmation',
-        text: 'Thanks for creqting your account click the button below to confirm',
-        html: '<button>Confirm</button>',
-      };
-      sgMail.send(msg);
-        passport.authenticate("local")(req, res, () => {
-          req.flash("notice", "You've successfully signed in!");
-          res.redirect("/");
-        })
-      };
-    });
-
+console.log(this);
+    const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: req.body.email,
+    from: 'rimal.binaya@gmail.com',
+    subject: 'Email Confirmation',
+    text: 'Thanks for creqting your account click the button below to confirm',
+    html: `<button onclick = ${this.confirmedCreate(req,res,next)} >Confirm</button>`,
+  };
+  sgMail.send(msg);
   },
-
 }
