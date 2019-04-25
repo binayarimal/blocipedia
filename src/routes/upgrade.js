48b264fd633b7ec20de960cdc8bc7957a4476a31
Binaya Rimal <rimal.binaya@gmail.com>
2019-04-25 16:42:19 -0700

    upgraded

--- src/routes/upgrade.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const upgradeController = require("../controllers/upgradeController");
-router.post("/upgrade", upgradeController.upgrade)
-module.exports = router;
