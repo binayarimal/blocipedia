c67042f6d729c0c8dc4301c67889eab12b4fe8ea
Binaya Rimal <rimal.binaya@gmail.com>
2019-04-25 16:50:26 -0700

    bug fixed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,10 +3,10 @@ module.exports = {
     const staticRoutes = require("../routes/static");
     const userRoutes = require("../routes/users");
     const wikiRoutes = require("../routes/wikis");
-    const upgradeRoutes = require("../routes/upgrade")
+  
   app.use(staticRoutes);
   app.use(userRoutes);
   app.use(wikiRoutes);
-  app.use(upgradeRoutes)
+
   }
 }
