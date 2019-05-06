6f3438f20cc0dd704b1c78db5fff6e5d35d7eee1
Binaya Rimal <rimal.binaya@gmail.com>
2019-05-05 18:09:41 -0700

    changes made

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -70,16 +70,18 @@ show(req,res, next){
       console.log(req.params.id)
     }
     else{
+      let wiki = {
+        title:markdown.toHTML(thisWiki.title),
+        body:markdown.toHTML(thisWiki.body),
+        id:thisWiki.id,
+        userId:thisWiki.userId,
+        state:thisWiki.state,
+        private:thisWiki.isPrivate(),
+        collaborators:false}
+     if(req.user){
       collabQueries.getCollaborators(req.user.id, (err,collaborators)=>{
 
-        let wiki = {
-          title:markdown.toHTML(thisWiki.title),
-          body:markdown.toHTML(thisWiki.body),
-          id:thisWiki.id,
-          userId:thisWiki.userId,
-          state:thisWiki.state,
-          private:thisWiki.isPrivate(),
-          collaborators:false}
+
           if(err){
             console.log(err)
             req.flash("error", "unable to get collabs")
@@ -89,6 +91,9 @@ show(req,res, next){
             res.render("wikis/show", {wiki})
           }
         })
+      } else {
+        res.render("wikis/show", {wiki})
+      }
       }
     })
 
